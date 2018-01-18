dc5274de5517c2f06de95589e37f554b1e2dd41a
Malinna Leach <leachm03@GBBQBQH001M0135.local>
2018-01-18 18:58:07 +0000

    ML integrated with database

--- pub_app/src/app/components/kf-note-input/kf-note-input.component.ts
+++ pub_app/src/app/components/kf-note-input/kf-note-input.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-kf-note-input',
@@ -10,20 +11,56 @@ export class KfNoteInputComponent implements OnInit {
 
     public rating: number;
     public noteId: number;
-    public note: string;
+    public noteText: string;
+    public currentNote: any;
 
-    constructor(private route: ActivatedRoute) { }
+    constructor(
+        private route: ActivatedRoute,
+        private router: Router,
+        private http: HttpClient,
+    ) { 
+    }
 
     ngOnInit() {
         if (this.route.snapshot.queryParams['rating']) {
             this.rating = +this.route.snapshot.queryParams['rating'];
         } else if (this.route.snapshot.queryParams['noteId']) {
-            this.noteId = +this.route.snapshot.queryParams['noteId'];
+            let id = this.route.snapshot.queryParams['noteId'];
+            this.http.get('http://fandanzle.co.uk/api/moods/id/' + id).subscribe(data => {
+                this.currentNote = data['results'];
+                this.noteText = this.currentNote.note_txt;
+            });
+        }
+    }
+
+    actionNote() {
+        if (this.route.snapshot.queryParams['rating']) {
+            this.sumbitNote();
+        } else {
+            this.editNote(this.currentNote);
         }
     }
 
     sumbitNote() {
-        console.log({'rating': this.rating, 'note': this.note});
+        let body = {
+            "user": "5a609d93ad9b2a3b007031b9",
+            "mood": this.rating,
+            "note": this.noteText
+        }
+        this.http.post('http://fandanzle.co.uk/api/moods', body).subscribe(data => {
+            this.router.navigate(['/notes']);
+        });
     }
+
+    editNote(note) {
+        let body = {
+          "user": "5a609d93ad9b2a3b007031b9",
+          "mood": note.mood,
+          "note": this.noteText
+      }
+        this.http.post('http://fandanzle.co.uk/api/moods/id/' + note._id, body).subscribe(data => {
+          this.router.navigate(['/notes']);
+      });
+      }
 
 }
