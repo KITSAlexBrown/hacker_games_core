33db81e00f1be5f09c60d315c7239fd35f392384
ahmed.shehab <ahmed.shehab@kingfisher.com>
2018-01-18 11:18:34 +0000

    adds basic home page and bootstrap functionality

---
+++ pub_app/src/app/components/home/kf-home.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'kf-home',
+    templateUrl: './kf-home.component.html',
+    styleUrls: ['./kf-home.component.scss']
+})
+
+export class KfHomeComponent {
+
+}
