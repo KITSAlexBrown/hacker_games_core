f40d0b2473b7cb8f2cb7dffdf53868e6fdd74543
Silvana Donato <silvana.donato@accenture.com>
2018-01-19 15:26:31 +0000

    preset user

--- pub_app/src/app/providers/login.service.ts
+++ pub_app/src/app/providers/login.service.ts
@@ -8,7 +8,12 @@ export class LoginService {
   private _userLogged: boolean = false;
   public user: User;
 
-  constructor(private usersService: UsersService) { }
+  constructor(private usersService: UsersService) {
+      this.usersService.getUsers().subscribe(users => {
+        this.user = users[0];
+        console.log("USER", this.user.name);
+      });
+  }
 
   public get userLogged(): boolean {
     return this._userLogged;
@@ -20,7 +25,7 @@ export class LoginService {
       let found = users.filter(user => user.email === email);
       if (found.length > 0) {
         this._userLogged = true;
-        this.user = found[0];
+        // this.user = found[0];
         return Observable.of(true);
       } else {
         return Observable.of(false);
